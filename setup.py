a27bea03bc43d7dcf965193741a85bfc0440f978
retagaine <eniagater@gmail.com>
2020-01-12 13:43:14 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='flsd',
+      version='1.0',
+      description='FLSD-Sandbox',
+      author='Sam Sinai',
+      packages=find_packages(),
+     )
