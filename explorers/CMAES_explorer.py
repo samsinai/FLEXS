950bc8b637d6ef508c93a6df7fdc79e38b22f99b
retagaine <eniagater@gmail.com>
2020-01-29 22:12:24 -0500

    update CMAES

--- explorers/CMAES_explorer.py
+++ explorers/CMAES_explorer.py
@@ -18,7 +18,8 @@ class CMAES_explorer(Base_explorer):
                          path,
                          debug)
         self.explorer_type = f"CMAES"
-        self.lam = 10
+        self.lam = self.batch_size
+        self.round = 0
         
     def initialize_params(self):
         # to be called after set_model
@@ -29,19 +30,24 @@ class CMAES_explorer(Base_explorer):
         # we'll be working with one-hots
         N = self.seq_len * self.alphabet_len
         self.N = N
-        self.mean = np.zeros(N)
-        self.sigma = 1
-        self.cov = np.identity(N)
-        self.p_sigma = np.zeros(N)
-        self.p_c = np.zeros(N)
-        
-        # approximation
-        self.c_sigma = 3/N
-        self.c_c = 4/N
-        self.d_sigma = 1
-        
         self.mu = self.lam//2
-        self.mu_w = self.lam//4
+        self.weights = [np.log(self.mu+0.5)-np.log(i) for i in range(1, self.mu+1)]
+        self.weights /= sum(self.weights)
+        self.mueff = sum(self.weights)**2/sum([w**2 for w in self.weights])
+        
+        self.c1 = 2/((self.N+1.3)**2+self.mueff)
+        self.cc = (4+self.mueff/N)/(N+4+2*self.mueff/N)
+        self.cs = (self.mueff+2)/(N+self.mueff+5)
+        self.cmu = min(1-self.c1, 2*(self.mueff-2+1/self.mueff)/((N+2)**2+self.mueff))
+        self.damp = 1 + 2*max(0, np.sqrt((self.mueff-1)/(N+1))-1) + self.cs
+        
+        self.mean = np.zeros(N)
+        self.sigma = 0.3
+        self.cov = np.identity(N)
+        self.ps = np.zeros(N)
+        self.pc = np.zeros(N)
+        
+        self.chiN = np.sqrt(N)*(1-1/(4*N)+1/(21*N**2))
         
     def convert_mvn_to_seq(self, mvn):
         # converts multivariate normal to one hot
@@ -67,49 +73,46 @@ class CMAES_explorer(Base_explorer):
     def compute_new_mean(self, samples):
         s = np.zeros(self.mean.shape)
         
-        # this is actually pretty bad, there's some recombination stuff going on here that I haven't done
         for i in range(self.mu):
-            s += samples[i][0]
+            s += self.weights[i-1]*samples[i][0]
             
-        s /= self.mu
         self.mean = s
         
     def expectation(self):
         return np.sqrt(self.N)*(1-1/(4*self.N)+1/(21*self.N**2))
         
-    def update_isotropic_evolution_path(self, old_mean, new_mean):
-        self.p_sigma = (1-self.c_sigma)*self.p_sigma + np.sqrt(1-(1-self.c_sigma)**2)*np.sqrt(self.mu_w)*np.linalg.inv(np.sqrt(self.cov))*(new_mean-old_mean)/self.sigma
+    def update_isotropic_evolution_path(self):
+        self.ps = (1-self.cs)*self.ps \
+            + np.sqrt(self.cs*(2-self.cs)*self.mueff)*np.linalg.inv(np.sqrt(self.cov))*(self.mean-self.old_mean)/self.sigma
         
     def update_step_size(self):
-        s = self.c_sigma/self.d_sigma
-        e = np.linalg.norm(self.p_sigma)/self.expectation()
-        self.sigma = self.sigma*np.exp(s*(e-1))
+        self.sigma = self.sigma*np.exp((self.cs/self.damp)*(np.linalg.norm(self.ps)/self.chiN - 1))
 
-    def p_sigma_indicator(self, squared=False):
-        alpha = 1.5
-        if squared:
-            return int(np.linalg.norm(self.p_sigma**2) <= alpha*np.sqrt(self.N))
-        return int(np.linalg.norm(self.p_sigma) <= alpha*np.sqrt(self.N))
+    def ps_indicator(self):
+        return int(np.linalg.norm(self.ps)/np.sqrt(1-(1-self.cs)**(2*self.round/self.lam))/self.chiN < 1.4 + 2/(self.N+1))
 
-    def update_anisotropic_evolution_path(self, old_mean, new_mean):
-        self.p_c = (1-self.c_c)*self.p_c + self.p_sigma_indicator()*np.sqrt(1-(1-self.c_c)**2)*np.sqrt(self.mu_w)*(new_mean-old_mean)/self.sigma
+    def update_anisotropic_evolution_path(self):
+        self.pc = (1-self.cc)*self.pc \
+            + self.ps_indicator()*np.sqrt(self.cc*(2-self.cc)*self.mueff)*(self.mean-self.old_mean)/self.sigma
 
-    def update_covariance_matrix(self, samples, old_mean):
-        c_1 = 2/(self.N**2)
-        c_mu = self.mu_w/(self.N**2)
-        c_s = (1 - self.p_sigma_indicator(squared=True))*c_1*self.c_c*(2-self.c_c)
-        weighted_sum = sum([((samples[i][0]-old_mean)/self.sigma)*((samples[i][0]-old_mean)/self.sigma).T for i in range(len(samples))])/self.mu
-        self.cov = (1-c_1-c_mu-c_s)*self.cov + c_1*(self.p_c*self.p_c.T) + c_mu
+    def update_covariance_matrix(self, samples):
+        weighted_sum = sum([self.weights[i-1]*((samples[i][0]-self.old_mean)/self.sigma)*((samples[i][0]-self.old_mean)/self.sigma).T for i in range(self.mu)])
+        
+        self.cov = (1-self.c1-self.cmu)*self.cov \
+            + self.c1*(self.pc*self.pc.T + (1-self.ps_indicator())*self.cc*(2-self.cc)*self.cov) \
+            + self.cmu*weighted_sum
 
     def propose_samples(self):
         # in CMAES we _minimize_ an objective, so I'll conveniently reverse
         samples = sorted(self._sample(), key=lambda s: s[1], reverse=True)
             
-        placeholder_mean = self.mean
+        self.old_mean = self.mean
         self.compute_new_mean(samples)
-        self.update_isotropic_evolution_path(old_mean=placeholder_mean, new_mean=self.mean)
-        self.update_anisotropic_evolution_path(old_mean=placeholder_mean, new_mean=self.mean)
-        self.update_covariance_matrix(samples, old_mean=placeholder_mean)
+        self.update_isotropic_evolution_path()
+        self.update_anisotropic_evolution_path()
+        self.update_covariance_matrix(samples)
         self.update_step_size()
+        
+        self.round += 1
         
         return [self.convert_mvn_to_seq(sample[0]) for sample in samples]
