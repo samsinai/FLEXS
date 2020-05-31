6ca2c7c92d511b74e9a941dd0ff5be21b7012a03
Alexander Whatley <alex@cresicor.com>
2020-05-30 23:54:19 -0500

    Added new greedy explorer for additive landscape.

---
+++ cloud/XE_IS_consistency_AAV.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.append("../")
+import RNA
+from utils.model_architectures import Linear, NLNN, CNNa
+from models.Noisy_models.Neural_network_models import NN_model
+from models.Ground_truth_oracles.RNA_landscape_models import RNA_landscape_constructor
+from models.Noisy_models.Ensemble import Ensemble_models
+from evaluators.Evaluator import Evaluator
+from models.Ground_truth_oracles.TF_binding_landscape_models import *
+from explorers.elitist_explorers import XE_IS
+
+LANDSCAPE_TYPES = {"Additive": ['heart', 'liver']
+}
+xeis_explorer_prod = XE_IS(recomb_rate=0.2, virtual_screen=20)
+xeis_explorer_prod.debug = False
+evaluator_xeis = Evaluator(
+    xeis_explorer_prod,
+    landscape_types=LANDSCAPE_TYPES,
+    path="../simulations/eval_xeis_consistency/",
+)
+evaluator_xeis.evaluate_for_landscapes(
+    evaluator_xeis.consistency_robustness_independence, num_starts=3
+)
