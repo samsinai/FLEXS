528e446733119bbeabfb83e50bd4880ec2f9d99d
Alexander Whatley <alex@cresicor.com>
2020-06-03 23:38:48 -0500

    Added WF explorers for AAV and GFP.

---
+++ cloud/WF_consistency_GFP.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append("../")
+import RNA
+from utils.model_architectures import Linear, NLNN, CNNa
+from models.Noisy_models.Neural_network_models import NN_model
+from models.Ground_truth_oracles.RNA_landscape_models import RNA_landscape_constructor
+from models.Noisy_models.Ensemble import Ensemble_models
+from evaluators.Evaluator import Evaluator
+from models.Ground_truth_oracles.TF_binding_landscape_models import *
+from explorers.evolutionary_explorers import WF
+
+LANDSCAPE_TYPES = {"GFP": [1, 5, 9]}
+xeis_explorer_prod = WF(recomb_rate=0.2, virtual_screen=20)
+xeis_explorer_prod.debug = False
+evaluator_xeis = Evaluator(
+    xeis_explorer_prod,
+    landscape_types=LANDSCAPE_TYPES,
+    path="../simulations/eval_wf_consistency/",
+)
+evaluator_xeis.evaluate_for_landscapes(
+    evaluator_xeis.consistency_robustness_independence, num_starts=3
+)
