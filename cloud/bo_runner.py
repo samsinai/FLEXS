d7aa37684c6fcc5f19fbd17459e2481509cb6ee7
Richard Wang <eniagater@gmail.com>
2020-09-30 17:11:32 -0700

    add bo tf table 1

--- cloud/bo_runner.py
+++ cloud/bo_runner.py
@@ -51,6 +51,39 @@ def run_bo_table1(landscape, wt, problem_name, start_num):
     results = _robustness(landscape, make_explorer)
     return results
 
+def run_bo_table1_tf(landscape, wt, problem_name, start_num):
+    alphabet = s_utils.DNAA
+
+    def _robustness(
+        landscape: flexs.Landscape,
+        make_explorer: Callable[[flexs.Model, float, str], flexs.Explorer]
+    ):
+        results = []
+
+        for ss in [0.0, 0.5, 0.9, 1.0]:
+            print(f"Evaluating for robustness with model accuracy; signal_strength: {ss}")
+
+            model = flexs.Ensemble([baselines.models.NoisyAbstractModel(landscape, signal_strength=ss)], combine_with=lambda x: x)
+            explorer = make_explorer(model, ss, tag=f"ss{ss}")
+            res = explorer.run(landscape, verbose=False)
+
+            results.append((ss, res))
+
+        return results
+        
+    def make_explorer(model, ss, tag):
+        return baselines.explorers.BO(
+            model=model,
+            rounds=10,
+            starting_sequence=wt,
+            sequences_batch_size=sequences_batch_size,
+            model_queries_per_batch=model_queries_per_batch,
+            alphabet=alphabet,
+            log_file=f"runs/bo/{problem_name}_start{start_num}_{tag}"
+        )
+    results = _robustness(landscape, make_explorer)
+    return results
+
 def run_bo_figure2a(landscape, wt, problem_name, start_num):
     alphabet = s_utils.DNAA
 
@@ -86,7 +119,7 @@ def run_bo_figure2a(landscape, wt, problem_name, start_num):
     return results
 
 if __name__ == "__main__":
-    task = "table1"
+    task = "table1_tf"
     if task == "figure2a":
         for p in ["POU3F4_REF_R1", "PAX3_G48R_R1", "SIX6_REF_R1", "VAX2_REF_R1", "VSX1_REF_R1"]:
             problem = flexs.landscapes.tf_binding.registry()[p]
@@ -101,3 +134,10 @@ if __name__ == "__main__":
             for s in range(5):
                 wt = problem["starts"][s]
                 results = run_bo_table1(landscape, wt, p, s)
+    elif task == "table1_tf":
+        for p in ["POU3F4_REF_R1", "PAX3_G48R_R1", "SIX6_REF_R1", "VAX2_REF_R1", "VSX1_REF_R1"]:
+            problem = flexs.landscapes.tf_binding.registry()[p]
+            landscape = flexs.landscapes.TFBinding(**problem["params"])
+            for s in range(13):
+                wt = problem["starts"][s]
+                results = run_bo_table1_tf(landscape, wt, p, s)
