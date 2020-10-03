4c39a839efc79e675c273bc6201554b5122a905d
Richard Wang <eniagater@gmail.com>
2020-10-03 14:12:17 -0700

    remove RNAFolding; remove baselines import

--- flexs/baselines/__init__.py
+++
@@ -1,1 +0,0 @@
-from flexs.baselines import explorers, models
--- flexs/landscapes/__init__.py
+++ flexs/landscapes/__init__.py
@@ -2,6 +2,6 @@
 from flexs.landscapes import rna  # noqa: F401
 from flexs.landscapes.additive_aav_packaging import AdditiveAAVPackaging  # noqa: F401
 from flexs.landscapes.bert_gfp import BertGFPBrightness  # noqa: F401
-from flexs.landscapes.rna import RNABinding, RNAFolding  # noqa: F401
+from flexs.landscapes.rna import RNABinding  # noqa: F401
 from flexs.landscapes.rosetta import RosettaFolding  # noqa: F401
 from flexs.landscapes.tf_binding import TFBinding  # noqa: F401
